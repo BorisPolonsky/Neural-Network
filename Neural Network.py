0aea98b28ec92ec3cec6110c8de38dda16a58f85
BorisPolonsky <qdogan@163.com>
2016-11-19 15:01:42 +0800

    Linear Function Approxmation Implemented
    The neural network can now approximate liear function y=Ax+b.
    Issues:
    Bug huanted.
    Ugly structure.
    Algorithm for hidden layers to be implemented.
    Data validation to be implemented.
    Other possible issues.

--- Neural Network.py
+++ Neural Network.py
@@ -17,7 +17,7 @@ class Neuron():
                 else:
                     self.__weight = weight[:]
             else:#Adjust settings for input layer
-                self.__input=float(Input)
+                self.__input = float(Input)
                 self.__weight = float(weight)
                 self.__threshold = 0.0
         else:#settings for input layers
@@ -32,18 +32,37 @@ class Neuron():
             else:
                 raise NeuronException
         else:#Adjust weight for input layer
-            self.__weight=float(weight)
+            self.__weight = float(weight)
+
+    def adjustThreshold(self,threshold):
+        self.__threshold = float(threshold)
+
+    def adjustGeneralizedWeight(self,GeneralizedWeight):#Generalized weight=weight+[threshold]
+        if len(GeneralizedWeight) != (len(self.__weight) + 1):
+            raise NeuronException
+        self.adjustWeight(GeneralizedWeight[:-1])
+        self.adjustThreshold(GeneralizedWeight[-1])
+
     def getWeight(self):
         return self.__weight[:]
-    def adjustThreshold(self,threshold):
-        if type(self.__input) == list:
-            self.__threshold = float(threshold)
+
+    def getThreshold(self):
+        return self.__threshold
+
+    def getGeneralizedWeight(self):
+        Generalizedweight = self.getWeight()
+        if type(Generalizedweight) == list:
+            Generalizedweight+=[self.getThreshold()]
+        else:
+            Generalizedweight = [Generalizedweight] + [self.getThreshold()]
+        return Generalizedweight
+
     def setInput(self,Input):
-        if type(self.__weight)==list and type(Input)==list and len(self.__weight)==len(Input):
-            self.__input[:]=Input
+        if type(self.__weight) == list and type(Input) == list and len(self.__weight) == len(Input):
+            self.__input[:] = Input
             return
         else:
-            self.__input=float(Input)
+            self.__input = float(Input)
             return
         raise NeuronException
         
@@ -51,8 +70,6 @@ class Neuron():
         if type(self.__input) == list:#Get output for hidden layers or output layer
             Output = 0
             for i in range(len(self.__input)):
-                if type(self.__input[i])!=Neuron:
-                    print("Error")
                 Output+=self.__weight[i] * self.__input[i].getOutput()
             Output+=self.__threshold
             #Output = sigmoid(Output)
@@ -71,43 +88,52 @@ class NeuralNetwork():
                 self.__Neurons.append([Neuron(Input=[self.__Neurons[-1][:]],threshold=0) for i in range(10)])
             self.__Neurons.append([Neuron(Input=self.__Neurons[-1][:],threshold=0) for i in range(Num_Output)])
 
-    def fit(self,input_data,output_data,num_hidden_layer=None,data_by_rows=True,TestDataRatio=0.2,max_step=100,epsilon=0.02):
+    def fit(self,input_data,output_data,num_hidden_layer=None,data_by_rows=True,TestDataRatio=0.2,max_step=200,Epsilon=0.02):
         if num_hidden_layer == None:
             num_hidden_layer = int(math.sqrt(len(input_data[0]))) + 5
+
+        num_hidden_layer = 0# algorithm for hidden layers to be implemented
+
+
         self.__generate(len(input_data[0]),len(output_data[0]),num_hidden_layer)
         #learning algorithm when there's no hidden layer
-        for i in range(len(self.__Neurons)-1):#Layer by layer
-            for j in range(len(self.__Neurons[-1-i])):#Neuron by neuron/Output by output
+        for i in range(len(self.__Neurons) - 1):#Layer by layer
+            for j in range(len(self.__Neurons[-1 - i])):#Neuron by neuron/Output by output
                 for step_count in range(max_step):#Step by step
-                    grad=[0 for n_w in range(len(self.__Neurons[-2-i]))]
-                    for k in range(len(grad)):#weight by weight
+                    grad = [0 for n_w in range(len(self.__Neurons[-2 - i]) + 1)]#gradient of Error with respect to generalizedweight.
+                    for k in range(len(grad)):#Calculate gradient weight by weight(generalzide)
                         for l in range(len(output_data)):#sample by sample
                             self.setInput(input_data[l])
-                            grad[k]=grad[k]+(self.__Neurons[-1-i][j].getOutput()-output_data[l][j])*1*self.__Neurons[-2-i][k].getOutput()#falty?
-                    weight=self.__Neurons[-i-1][j].getWeight()
-                    print("weight:",weight)
-                    print(grad)
-                    for k in range(len(grad)):#weight by weight(Update)
-                        weight[k]=weight[k]-self.__stepSize(step_count)*grad[k]
-                    print("New weight:",weight)
-                    self.__Neurons[-i-1][j].adjustWeight(weight)
+                            if k < (len(grad) - 1):#adjust kth weight
+                                grad[k] = grad[k] + (self.__Neurons[-1 - i][j].getOutput() - output_data[l][j]) * 1 * self.__Neurons[-2 - i][k].getOutput()#falty?
+                            else:#adjust threshold
+                                grad[k] = grad[k] + (self.__Neurons[-1 - i][j].getOutput() - output_data[l][j]) * 1 * 1
+                                #grad[k]=0
+                    Generalizedweight = self.__Neurons[-i - 1][j].getGeneralizedWeight()
+                    for k in range(len(grad)):#Updae weight by weight(Generalized)
+                        Generalizedweight[k] = Generalizedweight[k] - self.__stepSize(step_count) * grad[k]
+                    #print("New weight:",Generalizedweight[:-1])
+                    #print("New threshold",Generalizedweight[-1])
+                    self.__Neurons[-i - 1][j].adjustGeneralizedWeight(Generalizedweight)
+
     def __stepSize(self,n):
         #return 0.02*math.exp(-5*n)
-        return 0.02
+        return 0.005
+
     def setInput(self,data):
-        if len(data)==len(self.__Neurons[0]):
+        if len(data) == len(self.__Neurons[0]):
             for i in range(len(data)):
                 self.__Neurons[0][i].setInput(data[i])
             return
         raise NeuronException
 
     def getOutput(self,Input=None):
-        if Input!=None:
+        if Input != None:
             self.setInput(Input)
         return [Node.getOutput() for Node in self.__Neurons[-1]]
 
     def size(self):
-        size=0
+        size = 0
         for layer in self.__Neurons:
             size+=len(layer)
         return size
@@ -122,14 +148,14 @@ class NeuronException(Exception):
         pass
 
 def test():
-    a=NeuralNetwork()
-    Input=[[random.random(),random.random()] for i in range(100)]
-    Output=[]
+    a = NeuralNetwork()
+    Input = [[random.random(),random.random(),random.random()] for i in range(200)]
+    Output = []
     for input in Input:
-        Output.append([-3*input[0]+3*input[1]])
+        Output.append([input[0] + input[1] + input[2] + 10,input[1] + input[2] + 10,input[2] + 10])
     #y=2x1+3x2
-    a.fit(Input,Output,num_hidden_layer=0,max_step=50)
-    print(a.getOutput([8,8]))
+    a.fit(Input,Output,num_hidden_layer=0,max_step=300)
+    print(a.getOutput([1,1,1]))
     del a
 if __name__ == "__main__":
     test()
